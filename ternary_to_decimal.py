bad0a3c10b7651eeac69de4b1f4b140dfc716554
Fikri Aufaa Zain <65524505+fikriazain@users.noreply.github.com>
2021-10-01 01:21:25 +0700

    Update ternary_to_decimal.py

--- ternary_to_decimal.py
+++ ternary_to_decimal.py
@@ -1,10 +1,9 @@
-def ternary_decimal_conv(number_input, n_base):
+def ternary_decimal_conv(number_input):
     '''Convert N base number to a decimal number
     Keyword:
-    number_input -- Giving a certain number from any N base number
-    n_base -- user input N base
+    number_input -- Giving a certain number from any ternary number
     return:
-    sum_num -- the sum of each digit of the power of N base
+    sum_num -- the sum of each digit of the power of ternary
     '''
     sum_num = 0
     string_user = str(number_input)
