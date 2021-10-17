00dc178e076310c91977db37c0d5b6ed38629d50
Sai Sahith <91070868+sai-sahith7@users.noreply.github.com>
2021-10-17 17:21:56 +0530

    Create sWAP cASE.py

---
+++ sWAP cASE.py
@@ -0,0 +1,10 @@
+text = input("Enter Text : ")
+res=""
+for i in text:
+  if i.isupper():
+    res = res + i.lower()
+  elif i.islower():
+    res = res + i.upper()
+  else:
+    res = res + i
+print("Text with swapped case : ",res)  
